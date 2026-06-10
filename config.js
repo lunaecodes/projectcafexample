594173f9c87aa53964075307cf42da7fbb74438d
Lunae Codes <157434361+lunaecodes@users.noreply.github.com>
2026-06-10 12:43:54 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// ==========================================================================
+// CONFIGURACIÓN DE CREDENCIALES DE SUPABASE
+// ==========================================================================
+
+
+const SUPABASE_URL = "https://cvwpowkrzulkrfbjtyuy.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2d3Bvd2tyenVsa3JmYmp0eXV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwOTU1NjcsImV4cCI6MjA5NjY3MTU2N30.jTGU2UzE48SaNP1Rkhe5fQDJqTU2DZ1EyOA5Z7ztxcY";
